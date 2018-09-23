815d80f697ef51343af86b5a0f10a18c5301ce5c
emmanoelf <emmanoel_95@hotmail.com>
2018-09-23 16:04:13 -0300

    [feat] Def MPSBR Controle modificadores, grupo de controle de configuração e branch

--- js/Lista-verbetes/geraVerbetes.js
+++ js/Lista-verbetes/geraVerbetes.js
@@ -14,10 +14,23 @@ let CCMMI_ControleModificadores = `
     As provisões para a condução do gerenciamento de configuração devem ser estabelecidas em contratos com fornecedores.
     Métodos para assegurar que os dados estejam completos e consistentes devem ser estabelecidos e mantidos.</i>
 </p>`;
+
+let MPSBR_ControleModificadores = `
+<p>Tem função de executar a função de controle da configuração de forma sistemática, armazenando todas as informações geradas durante o andamento das solicitações de modificação e relatando essas informações aos envolvidos, assim como estabelecido pela função de contabilização da situação da configuração.</p>`;
+
+let MPSBR_GrupoDeControleDeConfiguracao = `
+<p>Grupo de pessoas responsável por avaliar e aprovar ou rejeitar modificações propostas em itens de configuração, e certificar que as modificações aprovadas foram implementadas.</p>`;
+
+let MPSBR_Branch = `
+<p>É a definição de uma estratégia que permita desenvolvimento em paralelo sobre uma base única de programas fontes.</p>`;
+
 //==============================================================================================
 
 //==================================== Funções dos Verbetes ====================================
 $(document).ready(function(){
     $("#CMMI_ControleModificadores").html(CCMMI_ControleModificadores);
+    $("#MPSBR_ControleModificadores").html(MPSBR_ControleModificadores);
+    $("#MPSBR_GrupoDeControleDeConfiguracao").html(MPSBR_GrupoDeControleDeConfiguracao);
+    $("#MPSBR_Branch").html(MPSBR_Branch);
 })
 //==============================================================================================
